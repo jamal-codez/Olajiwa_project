6f5d6efe79522ff6b2b128162bd86b2039d2946a
jamal-codez <jamalazimabdullahi@gmail.com>
2023-06-30 08:27:20 +0300

    changes

--- stockcard/urls.py
+++ stockcard/urls.py
@@ -1,11 +1,13 @@
 from django import views
 from django.urls import path
+from django.contrib import admin
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
     path('', views.were, name='werey'),
+    path('admin/', admin.site.urls),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.home, name='home'),
