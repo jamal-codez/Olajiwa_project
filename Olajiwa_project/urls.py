b1acaf250b7a3bb572b18ea9a33e39a36bf36a46
Jamal Azim ABDULLAHI <135423558+jamal-codez@users.noreply.github.com>
2024-10-03 16:17:34 +0100

    Update urls.py

--- Olajiwa_project/urls.py
+++ Olajiwa_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cat', include('stockcard.urls'))
+    path('', include('stockcard.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
